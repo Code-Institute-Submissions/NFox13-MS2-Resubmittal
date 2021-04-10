19997240f5f47d9dedfda61e8a81e5d07f8613a4
NFox13 <n_fox2000@yahoo.com>
2021-04-10 14:34:47 +0000

    Playing with code for the suitable walks buttons to try to fix bugs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-//Suitable walks alert buttons
-/*function displayAlert1() {
-        alert("");
-    }
-
-function displayAlert2() {
-        alert("Hi! Walks 2 & 6 would be a suitable walk for you");
-    }
-
-function displayAlert3() {
-        alert("Hi! Walks 3 & 4 would be a suitable walk for you");
-    }*/
-
-//Display meesage trial
-
-const btn = document.querySelector('#msgBox1');
+const btn = document.querySelector('#msgBox');
 btn.onclick = displayMessage;
 
 function displayMessage() {
@@ -41,6 +26,55 @@ function displayMessage() {
     }
 }
 
+//Lake walking button
+/*const btn = document.querySelector('#msgBox2');
+btn.onclick = displayMessage;
+
+function displayMessage() {
+    const html = document.querySelector("html");
+
+    const panel = document.createElement("div");
+
+    panel.setAttribute("class", "msgBox2");
+    html.appendChild(panel);
+
+    const msg = document.createElement("p");
+    msg.textContent = "Hi! Walks 2 & 6 would be a suitable walk for you";
+    panel.appendChild(msg);
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "x";
+    panel.appendChild(closeBtn);
+
+    closeBtn.onclick = function() {
+        panel.parentNode.removeChild(panel);
+    }
+}
+
+//Hill walking button
+const btn = document.querySelector('#msgBox3');
+btn.onclick = displayMessage;
+
+function displayMessage() {
+    const html = document.querySelector("html");
+
+    const panel = document.createElement("div");
+
+    panel.setAttribute("class", "msgBox3");
+    html.appendChild(panel);
+
+    const msg = document.createElement("p");
+    msg.textContent = "Hi! Walks 3 & 4 would be a suitable walk for you";
+    panel.appendChild(msg);
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "x";
+    panel.appendChild(closeBtn);
+
+    closeBtn.onclick = function() {
+        panel.parentNode.removeChild(panel);
+    }
+}*/
 
 //Membership calculator
 function calculateSubTotal() {
