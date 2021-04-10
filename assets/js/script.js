21d18e2551a6909afe53d505708f81e83ddb4e7d
NFox13 <n_fox2000@yahoo.com>
2021-04-10 13:39:57 +0000

    Added dispaly messages to replace alerts.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,33 @@ function displayAlert3() {
         alert("Hi! Walks 3 & 4 would be a suitable walk for you");
     }
 
+//Display meesage trial
+
+const btn = document.querySelector('button');
+btn.onclick = displayMessage;
+
+function displayMessage() {
+    const html = document.querySelector("html");
+
+    const panel = document.createElement("div");
+
+    panel.setAttribute("class", "msgBox");
+    html.appendChild(panel);
+
+    const msg = document.createElement("p");
+    msg.textContent = "This is a message box";
+    panel.appendChild(msg);
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "x";
+    panel.appendChild(closeBtn);
+
+    closeBtn.onclick = function() {
+        panel.parentNode.removeChild(panel);
+    }
+}
+
+
 //Membership calculator
 function calculateSubTotal() {
 
