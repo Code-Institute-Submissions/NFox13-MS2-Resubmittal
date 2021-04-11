803de86efde5d16bb0559899c0ad5bba1a43d052
NFox13 <n_fox2000@yahoo.com>
2021-04-11 09:31:44 +0000

    Added structure and styled alert messages for the recommended walks section. This section is now completed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,79 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 })
 
-const btn = document.querySelector('#msgBox');
-btn.onclick = displayMessage;
 
-function displayMessage() {
-    const html = document.querySelector("html");
-
-    const panel = document.createElement("div");
-
-    panel.setAttribute("class", "msgBox");
-    html.appendChild(panel);
-
-    const msg = document.createElement("p");
-    msg.textContent = "Hi! Walks 1 & 5 would be a suitable walk for you";
-    panel.appendChild(msg);
-
-    const closeBtn = document.createElement("button");
-    closeBtn.textContent = "x";
-    panel.appendChild(closeBtn);
-
-    closeBtn.onclick = function() {
-        panel.parentNode.removeChild(panel);
-    }
+//Suitable walks button
+console.clear();
+const buttonsList = document.querySelectorAll('button'), closeBtnList = document.querySelectorAll('.closebtn');
+buttonsList.forEach(each => each.addEventListener('click', handleBtnClick));
+closeBtnList.forEach(each => each.addEventListener('click', handleCloseBtnClick));
+        
+function handleBtnClick(evt) {
+document.querySelectorAll('.alert').forEach(each => each.style.display = 'none');
+document.querySelector('#'+evt.target.dataset.msgId).style.display = 'block';
 }
-
-//Lake walking button
-/*const btn = document.querySelector('#msgBox2');
-btn.onclick = displayMessage;
-
-function displayMessage() {
-    const html = document.querySelector("html");
-
-    const panel = document.createElement("div");
-
-    panel.setAttribute("class", "msgBox2");
-    html.appendChild(panel);
-
-    const msg = document.createElement("p");
-    msg.textContent = "Hi! Walks 2 & 6 would be a suitable walk for you";
-    panel.appendChild(msg);
-
-    const closeBtn = document.createElement("button");
-    closeBtn.textContent = "x";
-    panel.appendChild(closeBtn);
-
-    closeBtn.onclick = function() {
-        panel.parentNode.removeChild(panel);
-    }
+        
+function handleCloseBtnClick(evt) {
+evt.target.parentElement.style.display = 'none';
 }
-
-//Hill walking button
-const btn = document.querySelector('#msgBox3');
-btn.onclick = displayMessage;
-
-function displayMessage() {
-    const html = document.querySelector("html");
-
-    const panel = document.createElement("div");
-
-    panel.setAttribute("class", "msgBox3");
-    html.appendChild(panel);
-
-    const msg = document.createElement("p");
-    msg.textContent = "Hi! Walks 3 & 4 would be a suitable walk for you";
-    panel.appendChild(msg);
-
-    const closeBtn = document.createElement("button");
-    closeBtn.textContent = "x";
-    panel.appendChild(closeBtn);
-
-    closeBtn.onclick = function() {
-        panel.parentNode.removeChild(panel);
-    }
-}*/
 
 //Membership calculator
 function calculateSubTotal() {
