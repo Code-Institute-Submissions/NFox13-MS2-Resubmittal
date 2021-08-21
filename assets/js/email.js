9cdae822d10796420fd5f1024cc705573e86b87d
NFox13 <n_fox2000@yahoo.com>
2021-08-21 16:33:58 +0000

    Added functionality to the contact form on contact page.

---
+++ assets/js/email.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_c0hs0xh", "template_0yv6mgh", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "from_number": contactForm.number.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
